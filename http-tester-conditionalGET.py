da0b1e08b8340b976f11a29f760166611e69bd32
Jeremy Haugen <jhaugen2004@gmail.com>
2013-05-03 17:47:39 -0700

    Cleaned up the code, fixed some bugs. All the test cases pass except the conditional get

--- http-tester-conditionalGET.py
+++ http-tester-conditionalGET.py
@@ -91,12 +91,16 @@ try:
     server1.start()
     dataFile = open('basic', "r")
     cdata = dataFile.read()
+    print "cdata"
+    print cdata
     r = False
     proxy = '127.0.0.1:'+pport
     conn = HTTPConnection(proxy)
     conn.request("GET", "http://127.0.0.1:" + sport1 + "/basic")
     resp = conn.getresponse()
     data = resp.read()
+    print "data1"
+    print data
     conn.close()
 
     time.sleep(3)
@@ -104,6 +108,8 @@ try:
     conn2.request("GET", "http://127.0.0.1:" + sport1 + "/basic")
     resp2 = conn2.getresponse()
     data2 = resp2.read()
+    print "data2"
+    print data2
     conn2.close()
 
     time.sleep(3)
@@ -111,6 +117,8 @@ try:
     conn3.request("GET", "http://127.0.0.1:" + sport1 + "/basic")
     resp3 = conn3.getresponse()
     data3 = resp3.read()
+    print "data3"
+    print data3
     conn3.close()
 
     time.sleep(6)
@@ -118,6 +126,8 @@ try:
     conn4.request("GET", "http://127.0.0.1:" + sport1 + "/basic")
     resp4 = conn4.getresponse()
     data4 = resp4.read()
+    print "data4"
+    print data4
     conn4.close()
 
 
